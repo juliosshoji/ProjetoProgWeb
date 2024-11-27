0b75284f1c38b66f12f68197d2b70a11c7549bfc
Júlio Shoji Zotovici Kobayashi <zotokoba@gmail.com>
2024-11-26 22:02:33 -0300

    Subindo tudo

---
+++ ProjetoProgWeb/TelaRanking.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <title>Histórico do Jogador</title>
+        <link rel="stylesheet" href="./StyleInformacoes.css">
+        <script src="./DinamicaJogo.js"></script>
+    </head>
+
+    <body>
+        <?php 
+            session_start();
+            if(!isset($_SESSION["username"])){
+                header("Location: ./TelaLogin.html");
+            }    
+        ?>
+        <div class="bloco">
+            <h1>Histórico de Partidas</h1>
+            <div id = "ranking"></div>
+            <div class="button-container">
+                <a href="./TelaJogo.html">
+                    <div id="exit-button" class="button">Sair</div>
+                </a>
+            </div>
+        </div>
+    </body>
+</html>
